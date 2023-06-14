e4e40b3e609a677f172bb5e862eed4b0d0991103
kenk2 <kennykimjr@gmail.com>
2023-06-13 23:48:14 -0700

    Change homepage, page rendering behavior and default global styles.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@kenk2/styles/globals.css";
+import { NavigationBar } from "@kenk2/components";
+import { Box } from "@mui/material";
 import type { AppProps } from "next/app";
+import "@kenk2/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component props={pageProps} />;
+  return (
+    <Box sx={{ margin: 0 }}>
+      <NavigationBar />
+      <Box
+        sx={{
+          margin: "0 auto",
+          padding: 2,
+          width: "100%",
+          maxWidth: "700px",
+        }}
+      >
+        <Component props={pageProps} />
+      </Box>
+    </Box>
+  );
 }
